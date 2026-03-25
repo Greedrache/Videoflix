f5ec18e8d92e2c4deefc886be197845996ef3a36
Greedrache <greedrache@gmail.com>
2026-03-25 11:01:16 +0100

    Video App created

---
+++ content/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Video
+
+
+
+@receiver(post_save, sender=Video)
+def create_lecture(sender, instance, created, **kwargs):
+    print('Signal received for Video model')
+    if created:
+        print('New object created')
--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'content.apps.ContentConfig',
   #  'corsheaders',
   #  'api',
     # 'drf_yasg',
