da22ec261e85d1f3a8dded9cef6d30a824d4a19c
Greedrache <greedrache@gmail.com>
2026-04-07 17:35:41 +0200

    db daten entfernt

--- backend.entrypoint.sh
+++ backend.entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-echo "Warte auf PostgreSQL auf $DB_HOST:$DB_PORT..."
+echo "Warte auf PostgreSQL auf $POSTGRES_HOST:$POSTGRES_PORT..."
 
 # -q für "quiet" (keine Ausgabe außer Fehlern)
 # Die Schleife läuft, solange pg_isready *nicht* erfolgreich ist (Exit-Code != 0)
-while ! pg_isready -h "$DB_HOST" -p "$DB_PORT" -q; do
+while ! pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -q; do
   echo "PostgreSQL ist nicht erreichbar - schlafe 1 Sekunde"
   sleep 1
 done
